73103359457c9c5571e1dfb13332bfa53a40d95e
Gyula Szakacs <gyula_szakacs@epam.com>
2026-04-22 15:33:57 +0200

    revert: remove t13_final_task from completed branch

--- t13_final_task/task/agent/clients/base_mcp_client.ts
+++
@@ -1,6 +0,0 @@
-import { McpToolModel } from "../models.js";
-
-export abstract class BaseMcpClient {
-  abstract getTools(): Promise<McpToolModel[]>;
-  abstract callTool(toolName: string, toolArgs: Record<string, unknown>): Promise<string>;
-}
